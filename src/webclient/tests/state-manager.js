6a5c71f65b24432cc24ade311fbd82cb1a5cf328
OleksiiDoroshenko <43621504+OleksiiDoroshenko@users.noreply.github.com>
2020-05-03 14:10:53 +0300

    Refactor state manager tests

--- src/webclient/tests/state-manager.js
+++ src/webclient/tests/state-manager.js
@@ -1,45 +1,38 @@
 import AppService from '../main/services/app-secrvice';
-import UserData from '../models/user-data';
-import AuthorizationError from '../models/errors/authorization-error';
 import StateManager from '../main/services/state-manager';
-import ItemLoadingMutator from '../main/services/state-manager/mutators/item-loading-mutator';
-import ItemLoadingErrorMutator from '../main/services/state-manager/mutators/item-loading-error-mutator';
-import ItemsMutator from '../main/services/state-manager/mutators/get-item-mutator';
-import GetItemsAction from '../main/services/state-manager/actions/get-items';
+import Mutator from '../main/services/state-manager/mutators/mutator.js';
+import Action from '../main/services/state-manager/actions/action.js';
 
 const {module, test} = QUnit;
 
-export default module('State manager actions test', function(hook) {
-  let stateManager = new StateManager({}, new AppService(true));
+export default module('State manager test: ', function(hook) {
+  let stateManager = new StateManager({}, new AppService(false));
 
-  test('Should mutate its state', (assert) => {
-    let isLoadingMutator = new ItemLoadingMutator(true);
-    stateManager.mutate(isLoadingMutator);
-    assert.ok(stateManager.state.isLoading, 'Should assign true to the isLoading flag.');
-
-    isLoadingMutator = new ItemLoadingMutator(false);
-    stateManager.mutate(isLoadingMutator);
-    assert.notOk(stateManager.state.isLoading, 'Should assign false to the isLoading flag.');
-
-    const isLoadingErrorMutator = new ItemLoadingErrorMutator(new Error('Test'));
-    stateManager.mutate(isLoadingErrorMutator);
-    assert.strictEqual(stateManager.state.error.message, 'Test'
-      , 'Should assign error to the state.');
-
-    const itemsMutator = new ItemsMutator([{0: 'Test'}]);
-    stateManager.mutate(itemsMutator);
-    assert.strictEqual(stateManager.state.items.length, 1
-      , 'Should assign items to the state.');
+  test('should mutate its state', (assert) => {
+    let mutator = new Mutator();
+    mutator.apply = (state) => {
+      assert.ok(true, 'Should apply mutator.');
+    };
+    stateManager.mutate(mutator);
   });
 
-  test('Should dispatch action', async (assert) => {
-    const getItemsAction = new GetItemsAction(0);
-    stateManager = new StateManager({}, new AppService(true));
-    stateManager.onStateChanged('items', (state) => {
-      assert.strictEqual(state.items.length, 4
-        , 'Should dispatch get items action.');
+  test('should dispatch action', async (assert) => {
+    const action = new Action();
+    action.apply = (state) => {
+      assert.ok(true, 'Should apply action.');
+    };
+    await stateManager.dispatch(action);
+  });
+
+  test('should handle events', async (assert) => {
+    stateManager.onStateChanged('test', (state) => {
+      assert.ok(state.test, 'Should handle events');
     });
-    await stateManager.dispatch(getItemsAction);
-    assert.ok(true, '');
+    let mutator = new Mutator();
+    mutator.apply = (state) => {
+      state.test = true;
+    };
+    stateManager.mutate(mutator);
   });
+
 });

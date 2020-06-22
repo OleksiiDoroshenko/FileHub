a1492a9123a650941f61786c16f04355452a9e0f
OleksiiDoroshenko <43621504+OleksiiDoroshenko@users.noreply.github.com>
2020-06-22 18:27:49 +0300

    Add tests for registration method

--- server/src/test/java/io/javaclasses/fileHub/web/RegistrationTest.java
+++ server/src/test/java/io/javaclasses/fileHub/web/RegistrationTest.java
@@ -1,77 +1,57 @@
 package io.javaclasses.fileHub.web;
 
-import io.javaclasses.filehub.api.registrationProcess.Registration;
-import io.javaclasses.filehub.api.registrationProcess.Token;
 import io.javaclasses.filehub.api.registrationProcess.Register;
+import io.javaclasses.filehub.api.registrationProcess.Registration;
+import io.javaclasses.filehub.storage.userStorage.UserId;
+import io.javaclasses.filehub.storage.userStorage.UserRecord;
+import io.javaclasses.filehub.storage.userStorage.UserStorage;
+import io.javaclasses.filehub.web.InvalidUserDataException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("ExecutorTest should: ")
 class RegistrationTest {
+    private final UserStorage storage = new UserStorage();
+    private final Registration process = new Registration(storage);
 
-    private final Registration process = new Registration();
-
-    @DisplayName("log in user with correct data")
-    @Test
-    private void logInTest() {
-        String login = "Test";
-        String password = "Tests";
-        Register register = new Register(login, password);
-        Token token = process.logIn(register);
-    }
-
-    @DisplayName("not log in user with invalid data")
-    @Test
-    private void logInWithInvalidDataTest() {
-        String login = "Test";
-        String password = "Tests";
-        Register register = new Register(login, password);
-        assertThrows(IllegalArgumentException.class, () -> {
-            process.logIn(register);
-        }, "should throw exception when user data is invalid.");
-    }
 
     @DisplayName("register user with correct data")
     @Test
-    private void registerTest() {
-        String login = "Test";
-        String password = "Tests";
+    void registerTest() {
+        String login = "Tests";
+        String password = "Testsdkashdlkanfl";
         Register register = new Register(login, password);
-        User user = process.register(register);
-        assertEquals("registration method should return user with proper data"
-                , register, user.data());
+        UserId id = process.handle(register);
+        UserRecord record = storage.get(id);
+
+        assertEquals(record.getLogin(), login, "registration method should return user with proper login");
+
+        assertEquals(record.getPasswordHash(), String.valueOf(password.hashCode()),
+                "registration method should return user with proper password hash");
+
     }
 
     @DisplayName("register user with invalid data")
     @Test
-    private void registerWithInvalidDataTest() {
-        String login = "Test";
-        String password = "Tests";
-        Register register = new Register(login, password);
-        assertThrows(IllegalArgumentException.class, () -> {
-            process.register(register);
-        }, "should throw exception when user data is invalid.");
+    void registerWithInvalidDataTest() {
+
+        assertThrows(InvalidUserDataException.class, () -> {
+            String login = "Tes";
+            String password = "Testsdkashdjkjasklhdh";
+            new Register(login, password);
+
+        }, "crating register command with invalid login should throw exception when user login is to short.");
+
+        assertThrows(InvalidUserDataException.class, () -> {
+            String login = "Admin";
+            String password = "Tests";
+            new Register(login, password);
+
+        }, "crating register command with invalid password" +
+                " should throw exception when user password is to short.");
     }
 
-    @DisplayName("get user with correct token")
-    @Test
-    private void getUserTest() {
-        Token token = new Token("token");
-        User user = process.getUser(token);
-        assertEquals("getUser method should return user with proper token"
-                , token, user.token());
-    }
-
-    @DisplayName("get user with with invalid data")
-    @Test
-    private void getUserWithInvalidDataTest() {
-        Token token = new Token("token");
-        User user = process.getUser(token);
-        assertThrows(IllegalArgumentException.class, () -> {
-            process.getUser(token);
-        }, "should throw exception when token is invalid.");
-    }
 }

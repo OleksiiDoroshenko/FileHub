d2af8d0da645d195a25e896f63202cd0ea85955c
OleksiiDoroshenko <43621504+OleksiiDoroshenko@users.noreply.github.com>
2020-07-12 22:48:44 +0300

    Add JavaDocs

---
+++ server/src/main/java/io/javaclasses/filehub/api/folderCreationProcess/CreateFolder.java
@@ -0,0 +1,37 @@
+package io.javaclasses.filehub.api.folderCreationProcess;
+
+import io.javaclasses.filehub.api.Command;
+import io.javaclasses.filehub.storage.fileSystemItemsStorage.FileSystemItemId;
+import io.javaclasses.filehub.storage.fileSystemItemsStorage.FolderRecord;
+import io.javaclasses.filehub.storage.userStorage.UserId;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * The {@link Command} for creating new {@link FolderRecord}.
+ */
+public class CreateFolder implements Command {
+
+    private final FileSystemItemId parentId;
+    private final UserId ownerId;
+
+    /**
+     * Returns instance of {@link CreateFolder} class.
+     *
+     * @param parentId parent folder identifier.
+     * @param ownerId  owner identifier.
+     */
+    public CreateFolder(FileSystemItemId parentId, UserId ownerId) {
+
+        this.parentId = checkNotNull(parentId);
+        this.ownerId = checkNotNull(ownerId);
+    }
+
+    public FileSystemItemId parentId() {
+        return parentId;
+    }
+
+    public UserId ownerId() {
+        return ownerId;
+    }
+}
